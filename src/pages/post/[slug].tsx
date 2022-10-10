7e8cbbc5fd5bf7b47e00fe52a776a61162cd8c28
name <email>
2022-10-10 22:37:45 +0100

    fixed comments on comments schema, forgot to add actual comment data field

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -4,6 +4,8 @@ import prisma from "../../lib/prisma";
 import Image from "next/image";
 
 import { IoMdSend } from "react-icons/io"
+import { BsFillReplyFill } from "react-icons/bs"
+
 import { ChangeEvent, useState } from "react";
 import type { FormEvent } from "react";
 import axios, { AxiosError } from "axios";
@@ -15,6 +17,27 @@ import { User } from "@prisma/client";
 export default function Post({ post }: { post: any }) {
   const [resMsg, setResMsg] = useState<IResponseMessage>({ msg: '', err: false, pen: false })
 
+
+  const commentOnComment = async (commentId:string) => {
+    try {
+      setResMsg({ msg: "", err: false, pen: true })
+      await axios({
+        method: "POST",
+        url: "/api/post/comment",
+        data: { comment: commentInput, postId: post.id, commentId }
+      })
+      setResMsg({ msg: "", err: false, pen: false })
+    } catch (e: AxiosError | any) {
+      if (axios.isAxiosError(e)) {
+        e.response ?
+          //@ts-ignore-error
+          (has(e.response, "data") ? setResMsg({ msg: e.response.data.msg, err: true, pen: false }) : setResMsg({ msg: `${e}`, pen: false, err: true }))
+          : setResMsg({ msg: `${e}`, pen: false, err: true })
+      }
+    }
+  }
+
+  const handleCommentForm = (e: FormEvent<HTMLFormElement>) => { e.preventDefault(); comment() }
   const comment = async () => {
     try {
       setResMsg({ msg: "", err: false, pen: true })
@@ -45,16 +68,17 @@ export default function Post({ post }: { post: any }) {
       <h3 className="text-xl mx-auto">{post.description}</h3>
       <p className="mx-auto text-center">{post.content}</p>
       {post.comments.map((comment: any) => <div className="w-full p-1 flex gap-2 items-center">
-        <div className="relative w-6 h-6 overflow-hidden rounded-full">
+        <div className="relative w-8 h-8 overflow-hidden rounded-full">
           <Image src={comment.user.image} layout="fill" objectFit="cover" objectPosition="absolute" />
         </div>
         <div className="text-sm">{comment.comment}</div>
+        <BsFillReplyFill onClick={() => commentOnComment(comment.id)} className="cursor-pointer" />
       </div>)}
       {/* comment input */}
-      <div className="w-full flex h-20 items-center">
+      <form onSubmit={handleCommentForm} className="w-full flex h-20 items-center">
         <input value={commentInput} onChange={(e: ChangeEvent<HTMLInputElement>) => setCommentInput(e.target.value)} type="text" className="w-full border-b border-black h-8" />
-        <button onClick={() => comment()} className="cursor-pointer"><IoMdSend className="w-8 h-8" /></button>
-      </div>
+        <button type="submit" className="cursor-pointer"><IoMdSend className="w-8 h-8" /></button>
+      </form>
     </div>
   )
 }
@@ -73,12 +97,12 @@ export const getServerSideProps = async ({ params }: GetServerSidePropsContext) 
   });
   let out = JSON.parse(JSON.stringify(post))
   let uids: string[] = []
-  out.comments.forEach((comment:any) => { if(!uids.includes(comment.userId)) { uids.push(comment.userId) }})
-  const users = await Promise.all(uids.map((uid:string) => {
+  out.comments.forEach((comment: any) => { if (!uids.includes(comment.userId)) { uids.push(comment.userId) } })
+  const users = await Promise.all(uids.map((uid: string) => {
     return new Promise((resolve, reject) => {
-      prisma.user.findUnique({where: {id: uid}})
-      .then((user:any) => resolve(user))
-      .catch((e:any) => reject(e))
+      prisma.user.findUnique({ where: { id: uid } })
+        .then((user: any) => resolve(user))
+        .catch((e: any) => reject(e))
     })
   }))
   out.comments = post?.comments.map((comment: any) => {
