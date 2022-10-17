c12baddd59798c1860e3e01b4258c1b99124f8b9
name <email>
2022-10-17 16:09:50 +0100

    configuring redis for redis labs

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -1,11 +1,14 @@
 import {createClient} from "redis" 
 import type { RedisClientType } from "redis"
 let redisClient: RedisClientType | null = null
-redisClient = redisClient || createClient({
+redisClient = redisClient || createClient(process.env.NODE_ENV === "development" ? {
     socket: {
         host: process.env.REDIS_HOST,
         port: Number(process.env.REDIS_PORT)
     },
+} : {
+    url: process.env.REDIS_PRODUCTION_URL,
+    password: process.env.REDIS_PRODUCTION_PASS,
 })
 redisClient.on('error', (err) => {
     console.error(`${err}`)
