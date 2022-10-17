36e30c95584ef9d4589ab4d53814a07207a67c9a
name <email>
2022-10-17 16:53:22 +0100

    fixed redis connection url

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -7,8 +7,11 @@ redisClient = redisClient || createClient(process.env.NODE_ENV === "development"
         port: Number(process.env.REDIS_PORT)
     },
 } : {
-    url: process.env.REDIS_PRODUCTION_URL,
-    password: process.env.REDIS_PRODUCTION_PASS,
+    socket: {
+        host: process.env.REDIS_HOST,
+        port: Number(process.env.REDIS_PORT)   
+    },
+    password: process.env.REDIS_PASS,
 })
 redisClient.on('error', (err) => {
     console.error(`${err}`)
