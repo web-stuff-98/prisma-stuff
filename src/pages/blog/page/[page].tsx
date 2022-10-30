c856d5b306a0b5a12115cdb70d18c574ac049592
jordan claypole <jphoneacc98@gmail.com>
2022-10-30 23:48:05 +0000

    updated

--- src/pages/blog/page/[page].tsx
+++ src/pages/blog/page/[page].tsx
@@ -1,14 +1,14 @@
-import type { GetServerSideProps } from 'next'
-import Head from 'next/head'
-import { useRef } from 'react'
-import Post, { IPost } from '../../../components/post/Post'
+import type { GetServerSideProps, GetServerSidePropsContext } from "next";
+import Head from "next/head";
+import { useRef } from "react";
+import Post, { IPost } from "../../../components/post/Post";
 
-import prisma from '../../../utils/prisma'
+import prisma from "../../../utils/prisma";
 
-import { useEffect, useState } from 'react'
-import { useUsers } from '../../../context/UsersContext'
-import { useFilter } from '../../../context/FilterContext'
-import Link from 'next/link'
+import { useEffect, useState } from "react";
+import { useUsers } from "../../../context/UsersContext";
+import { useFilter } from "../../../context/FilterContext";
+import Link from "next/link";
 
 const Page = ({
   feed,
@@ -17,41 +17,44 @@ const Page = ({
   fullCount,
   maxPage,
 }: {
-  feed: any[]
-  popular: any[]
-  pageCount: number
-  fullCount: number
-  maxPage: number
+  feed: any[];
+  popular: any[];
+  pageCount: number;
+  fullCount: number;
+  maxPage: number;
 }) => {
-  const { cacheProfileDataForUser } = useUsers()
-  const { setPageCount, setFullCount, setMaxPage } = useFilter()
+  const { cacheProfileDataForUser } = useUsers();
+  const { setPageCount, setFullCount, setMaxPage } = useFilter();
 
   useEffect(() => {
-    if (!feed) return
-    feed.forEach((post: any) => cacheProfileDataForUser(post.author.id))
-  }, [feed])
+    if (!feed) return;
+    setFullCount(fullCount);
+    setPageCount(pageCount);
+    setMaxPage(maxPage);
+    let uids: string[] = [];
+    feed.forEach((post: any) => {
+      if (!uids.includes(post.author.id)) uids.push(post.author.id);
+    });
+    for (const uid of uids) {
+      cacheProfileDataForUser(uid);
+    }
+  }, [feed]);
+
+  const asideRootRef = useRef<HTMLDivElement>(null);
+  const [asideClientWidth, setAsideClientWidth] = useState(126);
 
   useEffect(() => {
-    setFullCount(fullCount)
-    setPageCount(pageCount)
-    setMaxPage(maxPage)
-  }, [feed])
-
-  const asideRootRef = useRef<HTMLDivElement>(null)
-  const [asideClientWidth, setAsideClientWidth] = useState(126)
-
-  useEffect(() => {
-    if (!asideRootRef.current) return
+    if (!asideRootRef.current) return;
     const resized = () => {
       if (asideRootRef.current)
-        setAsideClientWidth(asideRootRef.current?.clientWidth)
-    }
-    resized()
-    window.addEventListener('resize', resized)
+        setAsideClientWidth(asideRootRef.current?.clientWidth);
+    };
+    resized();
+    window.addEventListener("resize", resized);
     return () => {
-      window.removeEventListener('resize', resized)
-    }
-  }, [asideRootRef.current])
+      window.removeEventListener("resize", resized);
+    };
+  }, [asideRootRef.current]);
 
   return (
     <div className="flex relative justify-between w-full mx-auto pr-2 py-2">
@@ -72,7 +75,7 @@ const Page = ({
         >
           <div className="flex flex-col max-h-asideMaxHeight sm:overflow-y-auto shadow-sm rounded border sm:h-full gap-2 dark:border-zinc-800 pb-1">
             <h2
-              style={{ lineHeight: '1' }}
+              style={{ lineHeight: "1" }}
               className="font-sm font-ArchivoBlack py-2 sm:py-0 sm:pt-1 mx-auto"
             >
               Popular
@@ -81,7 +84,7 @@ const Page = ({
               popular.map((post: IPost) => (
                 <Link key={post.id} href={`/blog/post/${post.slug}`}>
                   <article
-                    style={{ lineHeight: '1' }}
+                    style={{ lineHeight: "1" }}
                     className="text-xs dark:hover:bg-zinc-800 hover:bg-gray-100 mx-1 rounded cursor-pointer flex flex-col"
                   >
                     <p>
@@ -94,26 +97,26 @@ const Page = ({
         </aside>
       </div>
     </div>
-  )
-}
+  );
+};
 
-import redisClient from '../../../utils/redis'
+import redisClient from "../../../utils/redis";
 
 export const getServerSideProps: GetServerSideProps = async ({
   params,
   query,
-}) => {
+}: GetServerSidePropsContext) => {
   try {
-    await redisClient?.connect()
+    await redisClient?.connect();
   } catch (e) {
-    console.error(`Could not connect to redis : ${e}`)
+    console.error(`Could not connect to redis : ${e}`);
   }
 
   // popular posts
-  let popular
-  const cachedPopularPosts = await redisClient?.get('popular')
+  let popular;
+  const cachedPopularPosts = await redisClient?.get("popular");
   if (cachedPopularPosts) {
-    popular = JSON.parse(cachedPopularPosts)
+    popular = JSON.parse(cachedPopularPosts);
   } else {
     popular = JSON.parse(
       JSON.stringify({
@@ -136,30 +139,30 @@ export const getServerSideProps: GetServerSideProps = async ({
           },
           orderBy: {
             likes: {
-              _count: 'desc',
+              _count: "desc",
             },
           },
           take: 8,
         }),
-      }),
-    )
-    const expiration: number = process.env.NODE_ENV === 'development' ? 5 : 120
-    await redisClient?.setEx('popular', expiration, JSON.stringify(popular))
+      })
+    );
+    const expiration: number = process.env.NODE_ENV === "development" ? 5 : 120;
+    await redisClient?.setEx("popular", expiration, JSON.stringify(popular));
   }
 
   /* The users parsed query parameters. Stored as keyname on redis so cached props can be looked up */
-  const { tags: rawTags } = query
-  const { term: rawTerm } = query
-  const clientQueryInput:any = {
+  const { tags: rawTags } = query;
+  const { term: rawTerm } = query;
+  const clientQueryInput: any = {
     pageOffset: Number(Math.max(Number(params?.page) - 1, 0) * 20),
     ...(rawTags
       ? {
           tags: rawTags
             ? String(rawTags)
                 .toLowerCase()
-                .split(' ')
-                .filter((tag: string) => tag.trim() !== '')
-                .map((tag: string) => tag.replace(/[^\w-]+/g, ''))
+                .split(" ")
+                .filter((tag: string) => tag.trim() !== "")
+                .map((tag: string) => tag.replace(/[^\w-]+/g, ""))
             : [],
         }
       : rawTerm
@@ -167,18 +170,18 @@ export const getServerSideProps: GetServerSideProps = async ({
           term: String(rawTerm)
             .toLowerCase()
             .trim()
-            .replaceAll('+', ' ')
-            .replace(/[^\w-]+/g, ''),
+            .replaceAll("+", " ")
+            .replace(/[^\w-]+/g, ""),
         }
       : {}),
-  }
+  };
 
-  const cachedProps = await redisClient?.get(JSON.stringify(clientQueryInput))
+  const cachedProps = await redisClient?.get(JSON.stringify(clientQueryInput));
   if (cachedProps) {
-    await redisClient?.disconnect()
+    await redisClient?.disconnect();
     return {
       props: { ...JSON.parse(cachedProps), ...popular },
-    }
+    };
   }
 
   //using type any because using mode:'sensitive' causes a typescript error for some reason
@@ -189,11 +192,11 @@ export const getServerSideProps: GetServerSideProps = async ({
           ? {
               title: {
                 contains: clientQueryInput.term,
-                mode: 'insensitive',
+                mode: "insensitive",
               },
             }
           : {}),
-      }
+      };
 
   const feedQ = await prisma.post.findMany({
     where: {
@@ -212,18 +215,18 @@ export const getServerSideProps: GetServerSideProps = async ({
       likes: true,
       shares: true,
       slug: true,
-      blur:true
+      blur: true,
     },
-    orderBy: { createdAt: 'desc' },
+    orderBy: { createdAt: "desc" },
     skip: clientQueryInput.pageOffset,
     take: 20,
-  })
+  });
   //add in the comment count.
   let outFeed: any[] = await Promise.all(
     feedQ.map(
       (p: any) =>
         new Promise((resolve, reject) => {
-          let cmtCount = 0
+          let cmtCount = 0;
           prisma.commentOnPost
             .findMany({
               where: { postId: p.id },
@@ -231,17 +234,17 @@ export const getServerSideProps: GetServerSideProps = async ({
             })
             .then((cmts) => {
               cmts.forEach((cmt) => {
-                cmtCount += cmt.CommentOnPostComment.length + 1
-              })
+                cmtCount += cmt.CommentOnPostComment.length + 1;
+              });
               resolve({
                 ...p,
                 comments: cmtCount,
-              })
+              });
             })
-            .catch((e) => reject(e))
-        }),
-    ),
-  )
+            .catch((e) => reject(e));
+        })
+    )
+  );
 
   // doing 2 queries, to count the total number of posts without page slice.....
   const feedQ_count = await prisma.post.findMany({
@@ -251,11 +254,11 @@ export const getServerSideProps: GetServerSideProps = async ({
       ...where,
     },
     select: { id: true },
-  })
+  });
   const feed = outFeed.map((data: any) => ({
     ...data,
     tags: data.tags.map((tag: any) => tag.name),
-  }))
+  }));
 
   const props = {
     feed: JSON.parse(JSON.stringify(feed)),
@@ -263,19 +266,19 @@ export const getServerSideProps: GetServerSideProps = async ({
     fullCount: feedQ_count.length,
     maxPage: Math.ceil(feedQ_count.length / 20),
     ...popular,
-  }
+  };
 
-  const key = JSON.stringify(clientQueryInput)
-  const expiration: number = process.env.NODE_ENV === 'development' ? 5 : 60
+  const key = JSON.stringify(clientQueryInput);
+  const expiration: number = process.env.NODE_ENV === "development" ? 5 : 60;
   await redisClient?.setEx(
     key,
     expiration,
-    JSON.stringify({ ...props, ...popular }),
-  )
+    JSON.stringify({ ...props, ...popular })
+  );
 
-  await redisClient?.disconnect()
+  await redisClient?.disconnect();
 
-  return { props }
-}
+  return { props };
+};
 
-export default Page
+export default Page;
