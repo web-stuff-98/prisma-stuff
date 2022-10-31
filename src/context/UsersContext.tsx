0211fc64432dd37e3b49764095107032b7b47fe2
jordan claypole <jphoneacc98@gmail.com>
2022-10-31 00:02:08 +0000

    forgot to save file

--- src/context/UsersContext.tsx
+++ src/context/UsersContext.tsx
@@ -1,51 +1,51 @@
-import { useContext, createContext, useState, useCallback } from 'react'
-import type { ReactNode } from 'react'
-import axios, { AxiosError } from 'axios'
+import { useContext, createContext, useState, useCallback } from "react";
+import type { ReactNode } from "react";
+import axios, { AxiosError } from "axios";
 
 export interface IUser {
-  name: string
-  image: string
-  id: string
-  createdAt: Date
+  name: string;
+  image: string;
+  id: string;
+  createdAt: Date;
 }
 
 const UsersContext = createContext<
   | {
-      findUserData: (uid: string) => IUser
-      cacheProfileDataForUser: (uid: string, force?: boolean) => void
+      findUserData: (uid: string) => IUser;
+      cacheProfileDataForUser: (uid: string, force?: boolean) => void;
     }
   | any
->(undefined)
+>(undefined);
 
 export default function UsersProvider({ children }: { children: ReactNode }) {
-  const [users, setUsers] = useState<IUser[]>([])
+  const [users, setUsers] = useState<IUser[]>([]);
 
   const findUserData = useCallback(
     (uid: string) => users.find((u: IUser) => u.id === uid),
-    [users],
-  )
+    [users]
+  );
 
   const cacheProfileDataForUser = async (uid: string, force?: boolean) => {
     try {
-      if (users.find((u: IUser) => u.id === uid) && !force) return
+      if (users.find((u: IUser) => u.id === uid) && !force) return;
       const axres = await axios({
-        method: 'GET',
+        method: "GET",
         url: `/api/user?uid=${uid}`,
-      })
-      setUsers([
-        ...users,
+      });
+      setUsers((old: IUser[]) => [
+        ...old,
         { ...axres.data, createdAt: new Date(axres.data.createdAt) },
-      ])
+      ]);
     } catch (e) {
-      console.error(e)
+      console.error(e);
     }
-  }
+  };
 
   return (
     <UsersContext.Provider value={{ findUserData, cacheProfileDataForUser }}>
       {children}
     </UsersContext.Provider>
-  )
+  );
 }
 
-export const useUsers = () => useContext(UsersContext)
+export const useUsers = () => useContext(UsersContext);
